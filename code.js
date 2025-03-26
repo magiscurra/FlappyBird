cfc1655094b320fc193819421116b81521ec9110
Akhilesh <69743694+magiscurra@users.noreply.github.com>
2025-03-26 09:34:27 +0530

    Update code.js

--- code.js
+++ code.js
@@ -15,7 +15,7 @@
         let gameRunning = true;
         let score = 0;
         let pipes = [];
-        let pipeGap = 200;
+        let pipeGap = 150;
         let pipeFrequency = 2000;    // Increased from 1500 ms
         let lastPipeTime = 0;
         
